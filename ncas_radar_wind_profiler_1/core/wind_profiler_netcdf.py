a6bb0c6da7a79a51450beeb0e00420aa91c5a1b0
Joshua Hampton <32854748+joshua-hampton@users.noreply.github.com>
2022-04-13 14:25:03 +0100

    Update file path and remove unnecessary comment

--- ncas_radar_wind_profiler_1/core/wind_profiler_netcdf.py
+++ ncas_radar_wind_profiler_1/core/wind_profiler_netcdf.py
@@ -70,7 +70,7 @@ calibration_certification_url = 'not applicable'
 
 
 # stuff for create_netcdf.main_vX
-netcdf_file_location = '/home/users/earjham/bin/writing_netcdf/test_nc_files'
+netcdf_file_location = '/gws/nopw/j04/ncas_obs/cdao/processing/ncas-radar-wind-profiler-1/netcdf_files'
 instrument_name = 'ncas-radar-wind-profiler-1'
 pi_scientist = {'name': 'Geraint Vaughan',
                 'primary_email': 'geraint.vaughan@manchester.ac.uk',
@@ -216,7 +216,6 @@ def main(all_trw_files, processing_software_version=processing_software_version,
     
                     
     # Can now add in values to time_coverage_start and time_coverage_end global attrs
-    #2016-07-06T00:00:00
     first_time = ncfile['time'][0]  # should be unix time
     first_time = dt.datetime.fromtimestamp(int(first_time))
     ncfile.time_coverage_start = first_time.strftime('%Y-%m-%dT%H:%M:%S')
@@ -244,7 +243,7 @@ def main(all_trw_files, processing_software_version=processing_software_version,
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(usage = '%(prog)s textfile [options]', description = "Create netCDF file for ncas-radar-wind-profiler-1. To edit more options, call the function main function from another python script.")
     parser.add_argument("trwfiles", default = None, nargs='+', help = "List of input trw files to create netCDF from")
-    parser.add_argument("-o", "--output-location", dest = "outloc", default = "/home/users/earjham/bin/writing_netcdf/test_nc_files", help = "Location for netCDF file")
+    parser.add_argument("-o", "--output-location", dest = "outloc", default = "/gws/nopw/j04/ncas_obs/cdao/processing/ncas-radar-wind-profiler-1/netcdf_files", help = "Location for netCDF file")
     
     args = parser.parse_args()
     
